3c172dffe999c77c072cc20c90b61105b5c7bf32
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-08-06 12:24:53 +0300

    Homework1

---
+++ Homeworks/Lesson1/main.js
@@ -0,0 +1,38 @@
+// 1
+const str1 = 'hello';
+const str2 = 'owu';
+const str3 = 'com';
+const str4 = 'ua';
+console.log(str1, str2, str3, str4)
+
+const num1 = 1;
+const num2 = 10;
+const num3 = -999;
+const num4 = 123;
+const num5 = 3.14;
+const num6 = 2.7;
+const num7 = 16;
+console.log(num1, num2, num3, num4, num5, num6, num7)
+
+const flag1 = true;
+const flag2 = false;
+console.log(flag1, flag2)
+
+// 2
+const firstName = 'Maxsim';
+const lastName = 'Dobrovolskyi';
+const middleName = 'Vitaliyovych';
+const person = `${lastName} ${firstName} ${middleName}`;
+console.log(person)
+
+// 3
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, typeof b, typeof c);
+
+// 4
+const fName = prompt('Enter your first name');
+const mName = prompt('Enter your middle name');
+const age = prompt('Enter your age');
+console.log(fName, mName, age)
